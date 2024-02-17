5416050276d5f4c0c920f779e0873f4cdebb334b
usagi <kou1992@gmail.com>
2024-02-18 04:24:00 +0900

    update

--- src/index.js
+++ src/index.js
@@ -8,3 +8,5 @@ console.log("test")
             console.log("test")
 
             var a = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 
+
+            console.log(a)
